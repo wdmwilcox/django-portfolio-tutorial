2cd456a1febdc3d1f87204bd80c84e6fed0cc307
David Wilcox <wdmwilcox@gmail.com>
2019-05-20 10:46:08 -0500

    functionality for blog

--- portfolio-project/blog/models.py
+++ portfolio-project/blog/models.py
@@ -4,3 +4,12 @@ class Blog(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+
+    def summary(self):
+        return self.content[:100]
+
+    def date_format(self):
+        return self.date.strftime('%b %e, %Y')
+
+    def __str__(self):
+        return self.title
---
+++ portfolio-project/blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from blog import views
+
+urlpatterns = [
+    path('', views.allblogs, name='allblogs'),
+    path('<int:blog_id>/', views.detail, name='detail'),
+]
